8324ffc02d970bc84c8fe65f8cd1f10904eb546f
f2xiao <fxiao.mecheng@gmail.com>
2022-03-05 14:49:37 -0500

    Add expressJS

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+// 1) MIDDLEWARES
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTES
+
+
+module.exports = app;
