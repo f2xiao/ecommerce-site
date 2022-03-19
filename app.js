4ff4cd7ac9cfb4692383a108fbbd4f6f7329783b
f2xiao <fxiao.mecheng@gmail.com>
2022-03-19 14:52:56 -0400

    Add MVC CRUD testing files

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const crudRouter = require('./routes/crud-route');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.engine('art', require('express-art-template'));
@@ -31,7 +32,7 @@ app.get('/', function (req, res) {
 });
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/crud', crudRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ routes/crud-route.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const crudController=require('../controllers/crud-controller');
+const router = express.Router();
+// curd form route
+router.get('/', crudController.crudForm );
+router.get('/form', crudController.crudForm );
+// create data route
+router.post('/create', crudController.createCrud);
+// display data route
+router.get('/fetch', crudController.fetchCrud);
+// edit data route
+router.get('/edit/:id', crudController.editCrud);
+// update data route
+router.post('/edit/:id', crudController.UpdateCrud);
+// delete data route
+router.get('/delete/:id', crudController.deleteCrud);
+module.exports = router;
